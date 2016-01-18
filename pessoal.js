8d1e8f912723257dcfe9b424552342837010398a
fatoonline <social@fatoonline.com.br>
2016-01-18 17:54:55 -0200

    Start
    Criação do código de ações

---
+++ pessoal.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+		$('body,html').animate({scrollTop: $(".header").offset().top}, "slow");
+		//ESQUENTA
+		$('#btn-esquenta').click(function(){
+		   $('body,html').animate({scrollTop: $(".esquenta").offset().top}, "slow"); 
+		});
+		//AGENDA
+		$('#btn-agenda').click(function(){
+		   $('body,html').animate({scrollTop: $(".agenda").offset().top}, "slow"); 
+		});
+		//---ITENS AGENDA
+		var removeClasse = function(){
+			$('.itens-agenda').removeClass('active');
+		}
+		$('.itens-agenda').mouseover(function(){
+		   $(this) .css('cursor','pointer');
+		});
+		$('.itens-agenda').click(function(){
+			removeClasse();
+			$(this).addClass('active');
+		})
+		//HISTORIA
+		$('#btn-historia').click(function(){
+		   $('body,html').animate({scrollTop: $(".historia").offset().top}, "slow"); 
+		});
+		//ALTERNATIVAS
+		$('#btn-alternativas').click(function(){
+		   $('body,html').animate({scrollTop: $(".alternativas").offset().top}, "slow"); 
+		});
+		//ESTRADA
+		$('#btn-estrada').click(function(){
+		   $('body,html').animate({scrollTop: $(".estrada").offset().top}, "slow"); 
+		});
+		//DICAS
+		$('#btn-dicas').click(function(){
+		   $('body,html').animate({scrollTop: $(".dicas").offset().top}, "slow"); 
+		});
+		//DICAS
+		$('#btn-dicas').click(function(){
+		   $('body,html').animate({scrollTop: $(".dicas").offset().top}, "slow"); 
+		});
+		//VIDEOS
+		$('#btn-videos').click(function(){
+		   $('body,html').animate({scrollTop: $(".videos").offset().top}, "slow"); 
+		});
+		//START
+		setTimeout(function(){
+		   $('.menu-lateral').animate({
+				"margin-left":"0px"
+			}) 
+		},2000);
+	});
