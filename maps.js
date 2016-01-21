91da5434b1f34b93b2010df9ccc480c7ca07d3fd
Fabricio Assis <fabricio.assis@fatoonline.com.br>
2016-01-21 14:36:24 -0200

    ponto central mapa
    ponto central mapa

--- maps.js
+++ maps.js
@@ -310,7 +310,7 @@ $(document).ready(function(){
   function agendaBrasilia() {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-      center: new google.maps.LatLng(-15.80722952, -47.98141479),
+      center: new google.maps.LatLng(-15.81047783, -47.98055649),
       zoom: 12  ,
       scrollwheel: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP
