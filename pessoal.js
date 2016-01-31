7a410e8d1d68134096a79da1bd6cda32da5feec2
FabricioAssis <fabricioassis@Macintosh.local>
2016-01-31 10:54:59 -0200

    cd
    cdew

--- pessoal.js
+++ pessoal.js
@@ -239,7 +239,7 @@ $(document).ready(function(){
 
 		//FOTOS
 		var fotos = [
-         "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/12642807_1023668780987243_8007148106614537869_n.jpg?oh=877a33b597c93184c983dfe07d6d35c0&oe=573462F1&__gda__=1462684616_c12472887142b636ccd35251c9ca539d",
+         "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/12642807_1023668780987243_8007148106614537869_n.jpg",
 
          "http://www.facebook.com/fatoonline/photos/a.1023668760987245.1073741838.839582699395853/1023668794320575",
 
